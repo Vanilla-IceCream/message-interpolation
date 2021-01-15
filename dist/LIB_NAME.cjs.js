17fd637250d73ecd2a44526dfd746f2fe3d67aec
Shyam-Chen <shyamchen1994@gmail.com>
2021-01-15 16:48:56 +0800

    14th Commit

--- dist/LIB_NAME.cjs.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var index = function index() {
-  var LIB_NAME = 'LIB_NAME';
-  return LIB_NAME;
-};
-
-module.exports = index;
--- dist/LIB_NAME.esm.js
+++
@@ -1,6 +0,0 @@
-var index = function index() {
-  var LIB_NAME = 'LIB_NAME';
-  return LIB_NAME;
-};
-
-export default index;
